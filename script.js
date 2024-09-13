3cc91b0a0d833b0f64dd6d58e9dc3ade95817111
FlugeheimenBot <floatingo086@gmail.com>
2024-09-13 21:36:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
